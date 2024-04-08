46f5431f886167380744d46a073bfafcdcaf909a
Sanarielsen <sanarielsen.dev@gmail.com>
2024-04-08 09:01:12 -0300

    feat(RMN-02): create post and get routes example
    Create post and get routes example in server.js file to know how to use
    that kind of routes and how can I sent data with meta-data to my client
    side.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 import http from 'node:http';
 
+const users = [
+  { id: 1, name: 'Lucas', email: 'lucas@email.com' },
+  { id: 2, name: 'João', email: 'joao@email.com' },
+  { id: 3, name: 'Maria', email: 'maria@email.com' }
+]
+
 const server = http.createServer((request, response) => {
-  return response.end('Hello World!');
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 4,
+      name: 'José',
+      email: 'zeze@mail.com'
+    })
+    
+    return response.writeHead(201).end()
+  }
+
+  return response.writeHead(404).end();
 });
 
 server.listen(3333);
